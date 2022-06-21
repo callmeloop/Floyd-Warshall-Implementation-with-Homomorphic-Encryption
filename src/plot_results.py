874afe394229c12f66101aee4ca28f314b33d688
İlker Sağlık <ilkersaglik.ee@gmail.com>
2022-06-21 22:47:18 +0300

    plots updated

--- src/plot_results.py
+++ src/plot_results.py
@@ -11,7 +11,7 @@ dir_name = 'plots/'
 
 width = 0.3
 # REDUCE TIMINGS
-fig_reduce, axs_reduce = plt.subplots(nrows=2, ncols=3, figsize=(12,6), constrained_layout=True)
+fig_reduce, axs_reduce = plt.subplots(nrows=3, ncols=2, figsize=(8,6), constrained_layout=True)
 fig_reduce.suptitle('Timings of the Floyd Warshall EVA Program for Various Input Sizes')
 
 # REDUCE FILES
@@ -25,21 +25,21 @@ mean = gb.mean()
 std = gb.std()
 x_pos = np.arange(len(gb_values))
 axs_reduce[0,0].bar(x_pos, mean['CompileTime'], width, yerr=std['CompileTime'], align='center', capsize=3, label=labels)
-axs_reduce[0,1].bar(x_pos, mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3, label=labels)
-axs_reduce[0,2].bar(x_pos, mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3, label=labels)
-axs_reduce[1,0].bar(x_pos, mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3, label=labels)
+axs_reduce[1,0].bar(x_pos, mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3, label=labels)
+axs_reduce[2,0].bar(x_pos, mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3, label=labels)
+axs_reduce[0,1].bar(x_pos, mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3, label=labels)
 axs_reduce[1,1].bar(x_pos, mean['DecryptionTime'], width, yerr=std['DecryptionTime'], align='center', capsize=3, label=labels)
-axs_reduce[1,2].bar(x_pos, mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3, label=labels)
+axs_reduce[2,1].bar(x_pos, mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3, label=labels)
 
 axs_reduce[0, 0].set_title('Compile Times')
-axs_reduce[0, 1].set_title('Key Generation Times')
-axs_reduce[0, 2].set_title('Encryption Times')
-axs_reduce[1, 0].set_title('Execution Times')
+axs_reduce[1, 0].set_title('Key Generation Times')
+axs_reduce[2, 0].set_title('Encryption Times')
+axs_reduce[0, 1].set_title('Execution Times')
 axs_reduce[1, 1].set_title('Decryption Times')
-axs_reduce[1, 2].set_title('Reference Execution Times')
+axs_reduce[2, 1].set_title('Reference Execution Times')
 
-for i in range(2):
-	for j in range(3):
+for i in range(3):
+	for j in range(2):
 		axs_reduce[i,j].legend(loc='best')
 plt.setp(axs_reduce, xlabel='Graph Node Size', ylabel='Time (ms)', xticks=x_pos, xticklabels=gb_values, xlim=(x_pos[0]-1.5*width, x_pos[-1]+1.5*width))
 # plt.show()
